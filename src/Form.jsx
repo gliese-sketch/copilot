3e15625b7dc621fc581e6a4ec0e52f08c6bf83b4
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-06 12:04:34 +0000

    Form Component

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 function Form() {
+  const [book, setBook] = useState(""); // For input
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Avoid page reload
+
+    console.log(book);
+    setBook(""); // Empty the input
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <fieldset>
-        <legend>Add a book:</legend>
+        <legend>Add a book</legend>
         <input
           type="text"
           name="quotes"
           id="quotes"
           placeholder="Enter a book"
+          autoComplete="off"
+          value={book}
+          onChange={(e) => setBook(e.target.value)}
         />
         <button type="submit">Add</button>
       </fieldset>
