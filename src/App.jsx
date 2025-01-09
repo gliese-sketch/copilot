0c2bab16d97ca2c532cdf42651f5edf2ca6ae6ff
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-01-09 12:28:21 +0000

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,38 @@ import { useReducer } from "react";
 import Form from "./Form";
 import QuotesList from "./QuotesList";
 
-const quotesReducer = () => {};
+const quotesReducer = (prevState, action) => {
+  switch (action.type) {
+    case "ADD":
+      const newQuote = {
+        id: crypto.randomUUID(),
+        text: action.payload,
+        likes: 1,
+      };
+      return [newQuote, ...prevState];
+    case "DELETE":
+      return prevState.filter((quote) => quote.id !== action.payload);
+    case "SORT":
+      return [...prevState].sort((a, b) => b.likes - a.likes);
+    case "LIKE":
+      return prevState.map((quote) =>
+        quote.id === action.payload
+          ? { ...quote, likes: quote.likes + 1 }
+          : quote
+      );
+    case "DISLIKE":
+      return prevState.map((quote) =>
+        quote.id === action.payload
+          ? { ...quote, likes: quote.likes - 1 }
+          : quote
+      );
+    default:
+      return prevState;
+  }
+};
 
 function App() {
   const [quotes, dispatch] = useReducer(quotesReducer, []);
-
-  // const addQuote = (quote) => {
-  //   const newQuote = {
-  //     id: crypto.randomUUID(),
-  //     text: quote,
-  //     likes: 1,
-  //   };
-
-  //   setQuotes([newQuote, ...quotes]);
-  // };
-
-  // const deleteQuote = (id) => {
-  //   setQuotes(quotes.filter((q) => q.id !== id));
-  // };
-
-  // const handleLike = (id) => {
-  //   setQuotes(
-  //     quotes.map((quote) =>
-  //       quote.id === id ? { ...quote, likes: quote.likes + 1 } : quote
-  //     )
-  //   );
-  // };
-
-  // const handleDislike = (id) => {
-  //   setQuotes(
-  //     quotes.map((quote) =>
-  //       quote.id === id ? { ...quote, likes: quote.likes - 1 } : quote
-  //     )
-  //   );
-  // };
-
-  // const quoteSort = () => {
-  //   setQuotes([...quotes].sort((a, b) => b.likes - a.likes));
-  // };
 
   return (
     <>
